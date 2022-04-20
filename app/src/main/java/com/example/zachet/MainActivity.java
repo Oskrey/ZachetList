4276a5e172a69a9e68e42ec2b332b03b115c3373
Никита <pedichn@gmail.com>
2022-04-20 20:29:27 +0300

    Комментарии

--- app/src/main/java/com/example/zachet/MainActivity.java
+++ app/src/main/java/com/example/zachet/MainActivity.java
@@ -18,17 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> list = new ArrayList<>();
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+        ArrayList<String> list = new ArrayList<>();                 //Создание списка
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, //Создание адаптера для списка
                 android.R.layout.simple_list_item_1, list );
+        ((ListView)findViewById(R.id.list)).setAdapter(adapter); //Установка списка в визуальный компонент
 
-        ((ListView)findViewById(R.id.list)).setAdapter(adapter);
+        int[] positionID = new int[1];  //Создание массива для хранения выбранного элемента
+        positionID[0] = -1;             //Установка значения на случай, если элемент не выбран
 
-        int[] positionID = new int[1];
-        positionID[0] = -1;
-
-        ((Button)findViewById(R.id.buttonAdd)).setOnClickListener(view -> {
+        ((Button)findViewById(R.id.buttonAdd)).setOnClickListener(view -> { //Создание действия на кнопку добавления
             AlertDialog.Builder bul = new AlertDialog.Builder(MainActivity.this);
             EditText input = new EditText(getApplicationContext());
             bul.setView(input).setMessage("Введите данные").setPositiveButton("Сохранить", (dialog, which) -> {
@@ -40,20 +38,21 @@ public class MainActivity extends AppCompatActivity {
             bul.show();
         });
 
-        ((ListView)findViewById(R.id.list)).setOnItemClickListener((parent, view, position, id) -> positionID[0] = (int) id);
+        ((ListView)findViewById(R.id.list)).setOnItemClickListener((parent, view, position, id) ->
+                positionID[0] = (int) id);//Получение ID элемента списка при клике на него
 
-        ((Button)findViewById(R.id.buttonDelete)).setOnClickListener(view -> {
-            if(positionID[0] != -1)
+        ((Button)findViewById(R.id.buttonDelete)).setOnClickListener(view -> {//Создание действия на кнопку удаления
+            if(positionID[0] != -1) //Если не невыбрано
             adapter.remove(list.get(positionID[0]));
-            positionID[0] = -1;
+            positionID[0] = -1;//Так как элемент удалён, ничего больше не выбрано
         });
 
-        ((Button)findViewById(R.id.buttonEdit)).setOnClickListener(view -> {
+        ((Button)findViewById(R.id.buttonEdit)).setOnClickListener(view -> {//Создание действия на кнопку редактирования
             if(positionID[0] != -1) {
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 EditText input = new EditText(getApplicationContext());
-                input.setText(list.get(positionID[0]));
+                input.setText(list.get(positionID[0]));//ввод имеющегося текста в поле ввода
                 builder.setView(input).setMessage("Изменение данных").setPositiveButton("Сохранить", (dialog, which) -> {
                     if (!input.getText().toString().isEmpty()) {
                         list.set(positionID[0], input.getText().toString());
